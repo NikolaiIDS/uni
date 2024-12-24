f56eb74886e51717fa231a680793b29cdd305160
Nikolay Simeonov <nikolaisimeonov60@gmail.com>
2024-12-24 18:26:32 +0200

    display all added and input fixed

--- DSR/DSR/DSR/DSR.cpp
+++ DSR/DSR/DSR/DSR.cpp
@@ -22,6 +22,37 @@ struct Request {
 	RequestStatus status;
 };
 
+
+
+void DisplayAllDevices(Request reqArray[], int const inputtedRequests)
+{
+	for (int i = 0; i < inputtedRequests; i++)
+	{
+		cout << "Request Number: " << reqArray[i].requestNumber << endl;
+		cout << "Date: " << reqArray[i].date << endl;
+		cout << "Client Name: " << reqArray[i].clientName << endl;
+		cout << "Device Type: " << reqArray[i].deviceType << endl;
+		cout << "Serial Number: " << reqArray[i].serialNumber << endl;
+		cout << "Issue Description: " << reqArray[i].issueDescription << endl;
+		cout << "Technician Name: " << reqArray[i].technicianName << endl;
+		cout << "Repair Details: " << reqArray[i].repairDetails << endl;
+		cout << "Repair Cost: " << reqArray[i].repairCost << endl;
+		cout << "Status: ";
+		if (reqArray[i].status == 1)
+		{
+			cout << "PENDING" << endl;
+		}
+		else if (reqArray[i].status == 2)
+		{
+			cout << "REJECTED" << endl;
+		}
+		else if (reqArray[i].status == 3)
+		{
+			cout << "COMPLETED" << endl;
+		}
+	}
+}
+
 void PrintMenu()
 {
 	cout << "----------------------------------" << endl;
@@ -43,53 +74,76 @@ int InputCommand(int& c)
 	return c;
 }
 
-Request InputRequest()
+void InputRequest(int& inputtedRequests, Request reqArray[])
 {
-	Request req;
+	int n = 0;
 
-	cout << "Enter Request Number: ";
-	cin >> req.requestNumber;
-	cin.ignore(); 
+	bool correctInput = false;
+	while (!correctInput)
+	{
+		cout << "Please enter the number of requests you want to input:";
+		cin >> n;
+		if (cin.fail() || n <= 0 || n > 100)
+		{
+			cin.clear();
+			cin.ignore();
+			cout << "Invalid input. Please enter a positive number less than or equal to 100." << endl;
+		}
+		else
+		{
+			correctInput = true;
+		}
+	}
 
-	cout << "Enter Date (DD-MM-YYYY): ";
-	cin.getline(req.date, MAX_STRING_LENGTH);
+	for (int i = 0; i < n; i++)
+	{
+		reqArray[i].requestNumber = inputtedRequests+1;
+		cout << "Request Number is: " << reqArray[i].requestNumber << endl;
 
-	cout << "Enter Client Name: ";
-	cin.getline(req.clientName, MAX_STRING_LENGTH);
+		cout << "Enter Date (DD-MM-YYYY): ";
+		cin.ignore();
+		cin.getline(reqArray[i].date, MAX_STRING_LENGTH);
 
-	cout << "Enter Device Type (e.g., Laptop, Phone): ";
-	cin.getline(req.deviceType, MAX_STRING_LENGTH);
+		cout << "Enter Client Name: ";
+		cin.getline(reqArray[i].clientName, MAX_STRING_LENGTH);
 
-	cout << "Enter Serial Number: ";
-	cin.getline(req.serialNumber, MAX_STRING_LENGTH);
+		cout << "Enter Device Type (e.g., Laptop, Phone): ";
+		cin.getline(reqArray[i].deviceType, MAX_STRING_LENGTH);
 
-	cout << "Enter Issue Description: ";
-	cin.getline(req.issueDescription, MAX_STRING_LENGTH);
+		cout << "Enter Serial Number: ";
+		cin.getline(reqArray[i].serialNumber, MAX_STRING_LENGTH);
 
-	cout << "Enter Technician Name: ";
-	cin.getline(req.technicianName, MAX_STRING_LENGTH);
+		cout << "Enter Issue Description: ";
+		cin.getline(reqArray[i].issueDescription, MAX_STRING_LENGTH);
 
-	cout << "Enter Repair Details: ";
-	cin.getline(req.repairDetails, MAX_STRING_LENGTH);
+		cout << "Enter Technician Name: ";		
+		cin.getline(reqArray[i].technicianName, MAX_STRING_LENGTH);
 
-	cout << "Enter Repair Cost: ";
-	cin >> req.repairCost;
-	cin.ignore(); 
+		cout << "Enter Repair Details: ";
+		cin.getline(reqArray[i].repairDetails, MAX_STRING_LENGTH);
 
-	cout << "Enter Status (0 for PENDING, 1 for REJECTED, 2 for COMPLETED): ";
-	int statusInput;
-	cin >> statusInput;
-	cin.ignore(); 
-	req.status = static_cast<RequestStatus>(statusInput);
+		cout << "Enter Repair Cost: ";
+		cin.ignore();
+		cin >> reqArray[i].repairCost;
 
-	cout << "\nRequest data successfully entered!" << endl;
+		cout << "Enter Status (0 for PENDING, 1 for REJECTED, 2 for COMPLETED): ";
+		int statusInput;
+		cin.ignore();
+		cin >> statusInput;
+		cin.ignore();
+		reqArray[i].status = static_cast<RequestStatus>(statusInput);
+
+		cout << "\nRequest data successfully entered!" << endl;
+		inputtedRequests++;
+	}
 }
 
 int main()
 {
 	PrintMenu();
-	Request reqArray[100];
-	int inputedRequests = 0;
+	const int MAX_REQUESTS = 100;
+	Request reqArray[MAX_REQUESTS];
+	int inputtedRequests = 0;
 	int command;
 	do
 	{
@@ -103,13 +157,13 @@ int main()
 		{
 			switch (command)
 			{
-				case 1: reqArray[inputedRequests] = InputRequest(); break;
-				case 2: break;
-				case 3: break;
-				case 4: break;
-				case 5: break;
-				case 6: break;
-				case 7: break;
+			case 1: InputRequest(inputtedRequests, reqArray); break;
+			case 2: DisplayAllDevices(reqArray, inputtedRequests); break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+			case 6: break;
+			case 7: break;
 			}
 		}
 	} while (command != 8);
